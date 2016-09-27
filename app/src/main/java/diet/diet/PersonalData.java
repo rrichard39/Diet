91897f4642435df5c303fa7f48bffcd9a3dfa937
Ron Richard <rrichard39@comcast.net>
2016-09-26 23:26:41 -0500

    Create

---
+++ app/src/main/java/diet/diet/PersonalData.java
@@ -0,0 +1,10 @@
+package diet.diet;
+
+/**
+ * Created by rrichard39 on 9/26/2016.
+ */
+
+public class PersonalData {
+    String name = "Ron Richard";
+    float height = 70.25f; /** in inches **/
+}
