874343103fe9c90da7c9a94563483ee377b2705b
Ron Richard <rrichard39@comcast.net>
2016-09-26 23:29:48 -0500

    Changed BMI formula to use PersonalData variable 'height'

--- app/src/main/java/diet/diet/Weight.java
+++ app/src/main/java/diet/diet/Weight.java
@@ -14,6 +14,7 @@ import java.util.Locale;
 public class Weight {
 
     WeightData WD;
+    PersonalData PD;
 
     Float goal = 0f;
     Date targetDate = new Date();
@@ -177,7 +178,7 @@ public class Weight {
                 WD.FirstRun = false;
             }
 
-            WD.BMI = (WD.LastWeight * 703f)/(70f * 70f);
+            WD.BMI = (WD.LastWeight * 703.0f)/(float)Math.pow(PD.height, 2);
         }
         catch (Exception e)
         {
