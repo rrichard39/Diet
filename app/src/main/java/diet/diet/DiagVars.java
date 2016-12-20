4e42c00cb4b1aac74eaac31a028fe09ede698ee9
Ron Richard <rrichard39@comcast.net>
2016-12-20 12:54:52 -0600

    Added descriptive comment

---
+++ app/src/main/java/diet/diet/DiagVars.java
@@ -0,0 +1,11 @@
+package diet.diet;
+
+/**
+ * Created by rrichard39 on 10/6/2016.
+ */
+
+public class DiagVars {
+    public static Integer oR_hits; //onResume
+    public static Integer oPR_hits; // onPostResume
+
+}
